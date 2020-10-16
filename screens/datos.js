2f32d8940326fef8893d9d8ba1630486af4a874d
RubenRock <ramses673@hotmail.com>
2020-10-16 08:05:18 -0500

    trabajando ventana datos

--- screens/datos.js
+++ screens/datos.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {View, TextInput, FlatList, Text, TouchableOpacity} from 'react-native'
+import React,{useState} from 'react'
+import {View, TextInput, FlatList, Text, TouchableOpacity, StyleSheet} from 'react-native'
 
 const productos = [
     {id:'1', producto:'azucar',precio:'20.5'},
@@ -8,21 +8,51 @@ const productos = [
 
 ]
 
+const empaques = [
+  {id:'1', id_producto:'1',empaque:'kg',precio:'20.5'},
+  {id:'2', id_producto:'1',empaque:'bulto',precio:'700'},
+  {id:'3', id_producto:'2',empaque:'250g',precio:'11'},
+  {id:'4', id_producto:'2',empaque:'caja',precio:'256'},
+  {id:'5', id_producto:'3',empaque:'ltr',precio:'17'},
+]
 
 function datosScreen() {
-    console.log(productos)
+    const [empaqueFiltrado,setempaqueFiltrado]= useState([])
+    
     return (
       <View >
           <TextInput placeholder='Producto'/>
           <FlatList 
+            style={styles.lists}
             data={productos}
-            renderItem={({item}) => <TouchableOpacity><Text >{item.id} - {item.producto}</Text></TouchableOpacity>}
-          />
-
-          
-         
+            renderItem={({item}) => <TouchableOpacity
+                onPress={ () => setempaqueFiltrado(empaques.filter(data => data.id_producto ==item.id ))}
+              >                
+                <Text >{item.id} - {item.producto}</Text>
+            </TouchableOpacity>}
+          />             
+          <FlatList 
+            style={styles.lists}
+            data={empaqueFiltrado}
+            renderItem={({item}) => <TouchableOpacity
+                  onPress={ () => console.log(item.precio)}
+                >                
+                <Text >{item.id} - {item.empaque}</Text>
+              </TouchableOpacity>}
+          />             
       </View>
     )
   }
 
   export default datosScreen
+
+const styles = StyleSheet.create({
+  lists:{
+    padding:5,
+    marginHorizontal:5,
+    marginTop:20,
+    height:200,
+    borderWidth:2,
+    borderColor:'black',
+  }
+})
