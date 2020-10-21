85a67499c2629bdedb00608a11073a6af3e4ae77
RubenRock <specterruben@gmail.com>
2020-10-21 17:16:45 -0500

    edicion de cantidad en remisiones arreglado

--- screens/remisiones.js
+++ screens/remisiones.js
@@ -5,15 +5,13 @@ function Remisiones({navigation, route}){
     const {dataTable} = route.params
 
     const [selectedValue, setSelectedValue] = useState('CONTADO')
-    const [currentDate, setCurrentDate] = useState('Cargando..');
-    const [counter, setCounter] = useState(1);    
+    const [currentDate, setCurrentDate] = useState('Cargando..');    
     const [table, setTable] = useState([]);    
+    const [total, setTotal] = useState(0)
 
 
-    useEffect(() => {               
-      console.log('cambio')
-      setTable(dataTable)
-      setCounter(1)
+    useEffect(() => {                 
+      setTable(dataTable)      
       var date = new Date().getDate(); //Current Date
       var month = new Date().getMonth() + 1; //Current Month
       var year = new Date().getFullYear(); //Current Year
@@ -24,49 +22,58 @@ function Remisiones({navigation, route}){
         date + '/' + month + '/' + year 
         + ' ' + hours + ':' + min + ':' + sec
       );           
-    },[route])     
-    
+    },[route]) 
+
     let data = [...table] 
+
+    //al realizar cualquier modificacion en "table" realizamos la suma 
+    useEffect( () => {
+      let total = data.map((el) => parseFloat(el.total))      
+      let  sum = total.reduce((prev, next) => prev + next,0)      
+      setTotal(sum)
+    },[table])
+    
+    
     const aumentar = (item,cant) =>{              
-      let id = (data.findIndex((x) => x.id == item.id))
-      setCounter(parseInt(cant)+1)      
-      data[id].cantidad = counter     
-      let newTable = table.map(el => el.id ==item.id ? {...el,cantidad:counter}: el)
-      console.log(counter)
-      setTable(newTable)
-      
+      let id = (data.findIndex((x) => x.id == item.id))      
+      data[id].cantidad = cant+1     
+      data[id].total = (cant+1)*data[id].precio
+      //let newTable = table.map(el => el.id ==item.id ? {...el,cantidad:cant+1}: el)      
+      setTable(data)      
     }
 
-    const disminur = item =>{          
-      let id = (data.findIndex((x) => x.id == item.id))
-      setCounter(counter-1)      
-      data[id].cantidad = counter      
+    const disminur = (item,cant) =>{          
+      let id = (data.findIndex((x) => x.id == item.id))      
+      data[id].cantidad = cant-1      
+      data[id].total = (cant-1)*data[id].precio
       setTable(data)
     }
     
 
     return (
       <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start',padding:10, backgroundColor:'white'}}>          
-      <View style={styles.header}>        
-        <View style={{flexDirection:'row', justifyContent:'space-around'}}>
-          <Button title="guardar" />
-          <Button title="limpiar" />
+        <View style={styles.header}>        
+          <View style={{flexDirection:'row', justifyContent:'space-around'}}>
+            <Button title="guardar" />
+            <Button title="limpiar" />
+          </View>
+          <TextInput placeholder="Nombre del cliente" style={styles.input}/>  
+          <TextInput placeholder="Domicilio" style={styles.input}/>  
+          <Picker           
+            selectedValue={selectedValue}
+            //style={{ height: 50, width: 150 }}
+            onValueChange={(itemValue) => setSelectedValue(itemValue)}
+          >
+            <Picker.Item label="CONTADO" value="CONTADO" />
+            <Picker.Item label="PAGA AL RECIBIR" value="PAGA AL RECIBIR" />
+            <Picker.Item label="CREDITO" value="CREDITO" />
+          </Picker>        
+          <Text>Fecha: {currentDate}</Text>          
+          <Button title='Productos' onPress={() => navigation.navigate('Datos')} />
         </View>
-        <TextInput placeholder="Nombre del cliente" style={styles.input}/>  
-        <TextInput placeholder="Domicilio" style={styles.input}/>  
-        <Picker           
-          selectedValue={selectedValue}
-          //style={{ height: 50, width: 150 }}
-          onValueChange={(itemValue) => setSelectedValue(itemValue)}
-        >
-          <Picker.Item label="CONTADO" value="CONTADO" />
-          <Picker.Item label="PAGA AL RECIBIR" value="PAGA AL RECIBIR" />
-          <Picker.Item label="CREDITO" value="CREDITO" />
-        </Picker>        
-        <Text>Fecha: {currentDate}</Text>
-        <Text>counter: {counter}</Text>
-        <Button title='Productos' onPress={() => navigation.navigate('Datos')} />
-      </View>
+
+        <Text>total: {total}</Text>
+
       <FlatList 
             style={styles.header}
             data={table}
@@ -75,8 +82,8 @@ function Remisiones({navigation, route}){
                 <View style={{flexDirection:'row', justifyContent:'space-between'}}>                
                   <Text  style={{flex:2}}>{item.cantidad} - {item.empaque} {item.producto}  ${item.precio}  ${item.total} </Text>             
                   <View style={{flex:1,flexDirection:'row',justifyContent:'space-between'}}>
-                    <Button title="+" onPress={() => aumentar(item,item.cantidad)}/>
-                    <Button title=" - " onPress={() => disminur(item)}/>
+                    <Button title="+" onPress={() => aumentar(item,parseInt(item.cantidad))}/>
+                    <Button title=" - " onPress={() => disminur(item,parseInt(item.cantidad))}/>
                     <Button title="borrar" onPress={() => setTable(table.filter((data)=> data.id !==item.id))}/>                                               
                   </View>                  
                 </View>
