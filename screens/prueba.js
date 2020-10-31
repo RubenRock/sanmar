9f09770b995f59f83d2e7f7ec230704e8753b0e2
RubenRock <ramses673@hotmail.com>
2020-10-31 08:06:54 -0600

    trabajando SQLITE

--- screens/prueba.js
+++ screens/prueba.js
@@ -1,6 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import * as SQLITE from 'expo-sqlite'
+import * as FileSystem from 'expo-file-system';
 
+console.log(FileSystem.documentDirectory)
+const db = SQLITE.openDatabase('../lib/NEWDB.s3db')
+
+console.log(db)
+
+
+
+/* useEffect(() => {
+  db.transaction(function (txn) {
+    txn.executeSql(
+      "SELECT name FROM sqlite_master WHERE type='table' AND name='table_user'",
+      [],
+      function (tx, res) {
+        console.log('item:', res.rows.length);
+        if (res.rows.length == 0) {
+          txn.executeSql('DROP TABLE IF EXISTS table_user', []);
+          txn.executeSql(
+            'CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY AUTOINCREMENT, user_name VARCHAR(20), user_contact INT(10), user_address VARCHAR(255))',
+            []
+          );
+        }
+      }
+    );
+  });
+}, []);
+ */
 function pruebaScreen({ navigation }) {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState(['orale']);
