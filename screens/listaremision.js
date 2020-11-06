3728ad35ebbf42a685fdb335a2ac00289f840b96
RubenRock <specterruben@gmail.com>
2020-11-06 14:11:21 -0600

    Configurando Lista Remisiones

--- screens/listaremision.js
+++ screens/listaremision.js
@@ -1,33 +1,89 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import {View, Text,FlatList, StyleSheet,TouchableOpacity, Button, TextInput} from 'react-native'
 import * as SQLITE from 'expo-sqlite'
 
 const db = SQLITE.openDatabase("db.db");
 
-const leerSql = (product, precio) => {
+
+
+
+
+function listaRemisionScreen(){
+  const [remisiones, setRemisiones]= useState([])
+  
+  useEffect (() => {
     db.transaction(
       tx => {               
-        tx.executeSql("select * from inventario", [],  (tx, res) =>  {            
+        tx.executeSql("select * from lista_remision", [],  (tx, res) =>  {            
             let resul = [];let index = 0
             while (index < res.rows.length) {
               resul = [...resul,res.rows.item(index)]
               index++              
             }    
-            console.log(resul)
-      },
-      (e) => console.log(e.message))
-  })
-}
-
-leerSql()
-
-function listaRemisionScreen(){
+            setRemisiones(resul)
+        },
+        (e) => console.log(e.message))
+      })
+    },[])
+    
     return(
         <View>
-            <Text>Hola desde lista Remision</Text>
+            <View style={styles.header}>
+              <View style={styles.boton}>
+                <Button  title="Mandar a la nube" onPress={() => console.log('ejecuta')}/>
+              </View>
+              <View>
+                <TextInput style={styles.input} placeholder="Folio inicial"/>
+                <TextInput style={styles.input} placeholder="Folio final"/>
+              </View>
+            </View>            
+
+            <FlatList             
+            data={remisiones}
+            keyExtractor={(ele) => ele.folio}
+            renderItem={({item}) =>
+                <View style={styles.lista}>
+                  <TouchableOpacity onPress={() => alert(item.folio) }>
+                    <Text style={styles.texto} >Folio: {parseInt(item.folio)}    Cliente: {item.cliente} </Text>
+                    <Text style={[styles.texto, styles.texto_pequeño]} >Total: ${item.total}   Fecha: {item.fecha}</Text>
+                    <Text style={[styles.texto, styles.texto_pequeño]} >Domicilio: {item.domicilio}</Text>
+                  </TouchableOpacity>
+                </View>
+              }
+            />            
         </View>
     )
 
 }
+
+const styles = StyleSheet.create({
+  lista:{    
+    padding:5,
+    backgroundColor:"#00AFF0",
+    marginTop:5,
+    marginHorizontal:5,   
+    borderRadius:10,
+  },
+  texto:{
+    color:'white',
+  },
+  texto_pequeño:{
+    fontSize:12
+  },
+  header:{
+    flexDirection:"row",
+    justifyContent:'flex-end'
+  },
+  input:{
+    borderBottomWidth:1,
+    marginTop:5,
+    marginHorizontal:10,
+  },
+  boton:{
+    margin:10
+  }
+
+
+})
 
 export default listaRemisionScreen
