d0fe7107ef6d4f3db96baf8fc155ace17e8b2039
RubenRock <ramses673@hotmail.com>
2020-11-08 11:16:01 -0600

    configurando mandar a la nube remisiones y listaremisiones

--- screens/listaremision.js
+++ screens/listaremision.js
@@ -5,54 +5,93 @@ import * as SQLITE from 'expo-sqlite'
 const db = SQLITE.openDatabase("db.db")
 
 function listaRemisionScreen(){
-  const [remisiones, setRemisiones]= useState([])
+  const [filtro, setFiltro]= useState(false)
+  const [remisiones, setRemisiones]= useState([])  
+  const [listaRemision, setListaRemision] = useState([])
+  const [data, setData]= useState([]) // listado de remisones de esta Screen
   const [folios, setFolios] = useState({inicio:'',fin:''})
+
 
  
 
   const mandarNube = async () => {
-    const responde = await fetch('https://vercel-api-eta.vercel.app/api/listaremision',{
-      method:'POST',
-      headers:{
-        'Content-Type':'application/json',
-      },
-      body:JSON.stringify(
-        {"folio":"8",
-        "cliente": "Pau",
-        "total": "100",
-        "fecha": "01/01/20",
-        "vendedor":"admin",
-        "condicion":"contado",
-        "estado":"pendiente",
-        "domicilio":"centro",
-        "impresion":"serie",
-        "descuento":"4"}
-      )
-    })
-
-    //const data = await responde.json()
-    if (responde.status !== 204) console.log('hay error:'+ responde.details)
-    else
-    console.log('esta bien')
     
+     
+     let index =folios.inicio
+    while (index <= folios.fin) {
+      const responde = await fetch('https://vercel-api-eta.vercel.app/api/listaremision',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body:JSON.stringify(
+          {"folio":listaRemision[index].folio,
+          "cliente": listaRemision[index].cliente,
+          "total": listaRemision[index].total,
+          "fecha": listaRemision[index].fecha,
+          "vendedor":listaRemision[index].vendedor,
+          "condicion":listaRemision[index].condicion,
+          "estado":listaRemision[index].estado,
+          "domicilio":listaRemision[index].domicilio,
+          "impresion":listaRemision[index].impresion,
+          "descuento":listaRemision[index].descuento}
+        )
+      })
+
+      if (responde.status == 204)  
+        console.log('exito: '+index) 
+      else {  // cuando hay error
+        const error = await responde.json()    
+        console.log('Hay error: '+ error.details)    
+      }    
+      
+
+      index++
+      
+    }
+     
+    
+  }
+  
+  function obtenerLista (){ 
+    let alisteRemision = []   
+    db.transaction(
+      tx => {
+        tx.executeSql("select * from lista_remision where folio >= ? and folio <= ?", [folios.inicio+'.0', folios.fin+'.0'],  (tx, res) =>  {            
+          let index = 0
+          while (index < res.rows.length) {            
+            alisteRemision.push(res.rows.item(index))
+            index++              
+          }    
+          
+          setListaRemision(alisteRemision)
+      },
+      (e) => console.log(e.message))
+    })
+  }
+
+  const obtenerRemisiones = () =>{
+    let aremision = []
+    db.transaction(
+      tx => {                                                                       //la bd le agrega .0 al folio
+         tx.executeSql("select * from remisiones where folio >= ? and folio <= ?", [folios.inicio+'.0', folios.fin+'.0'],  (tx, res) =>  {            
+            let index = 0
+            while (index < res.rows.length) {
+              aremision = [...aremision,res.rows.item(index)]
+              index++              
+            }    
+           
+            setRemisiones(aremision)
+        },
+        (e) => console.log(e.message))         
+      })
   }
 
   const handleBoton = () => {
-    db.transaction(
-      tx => {                                                                       //la bd le agrega .0 al folio
-        tx.executeSql("select * from remisiones where folio >= ? and folio <= ?", [folios.inicio+'.0', folios.fin+'.0'],  (tx, res) =>  {            
-            let resul = [];let index = 0
-            while (index < res.rows.length) {
-              resul = [...resul,res.rows.item(index)]
-              index++              
-            }    
-            console.log(resul)
-        },
-        (e) => console.log(e.message))
-        mandarNube()
-        
-      })
-
+      obtenerRemisiones()
+      obtenerLista()
+      setFiltro(true)
+      
   }
   
   useEffect (() => {
@@ -64,7 +103,7 @@ function listaRemisionScreen(){
               resul = [...resul,res.rows.item(index)]
               index++              
             }    
-            setRemisiones(resul)
+            setData(resul)
         },
         (e) => console.log(e.message))
       })
@@ -73,23 +112,35 @@ function listaRemisionScreen(){
     return(
         <View>
             <View style={styles.header}>
-              <View style={styles.boton}>
-                <Button  title="Mandar a la nube" onPress={() =>handleBoton()}/>
-              </View>
+              {folios.inicio <= folios.fin && folios.fin.trim() !== '' && folios.inicio.trim() !== '' && filtro == false?
+                <View style={styles.boton}>
+                  <Button  title="Seleccionar folios" onPress={() =>handleBoton()}/>                  
+                </View>
+                :null
+              }
+
+              {filtro?
+                <View style={styles.boton}>
+                    <Button color='red' title="Mandar a la nube" onPress={() =>mandarNube()}/>                  
+                  </View>
+                  :null
+              }
               <View>
                 <TextInput style={styles.input} 
                   placeholder="Folio inicial" 
                   onChangeText={(val) => setFolios({...folios, inicio:val})}
+                  keyboardType='numeric'                  
                 />
                 <TextInput style={styles.input} 
                   placeholder="Folio final"
                   onChangeText={(val) => setFolios({...folios, fin:val})}
+                  keyboardType='numeric'
                 />
               </View>
             </View>            
 
             <FlatList             
-            data={remisiones}
+            data={data}
             keyExtractor={(ele) => ele.folio}
             renderItem={({item}) =>
                 <View style={styles.lista}>
