4300f53739f1dde01f6805e8593ffa8748e3fb05
RubenRock <specterruben@gmail.com>
2020-11-23 17:17:58 -0600

    trabajando similaresScreen

--- screens/similares.js
+++ screens/similares.js
@@ -10,6 +10,7 @@ function similaresScreen({navigation, route}){
     const {cantidad, producto, empaque} = route.params  
 
     const [dataInventario, setDataInventario] = useState([])
+    const [completo, setCompleto] = useState(false)
     const [totalPiezas, setDatatotalPiezas] = useState('0')
     const [productoSeleccionado, setproductoSeleccionado] = useState('')    
     const [dataEmpaque, setDataEmpaque] = useState([])
@@ -18,11 +19,14 @@ function similaresScreen({navigation, route}){
     const [cant,setCant] = useState('1') 
 
     useEffect(() =>{
-      let total = listProductos.map((el) => parseInt(el.cantidad) * el.piezas)      
-      console.log(total)
+      cantidad * empaque.piezas == totalPiezas ? setCompleto(true)
+      : setCompleto(false)
+    },[totalPiezas])
+
+    useEffect(() =>{
+      let total = listProductos.map((el) => parseInt(el.cantidad) * el.piezas)            
       let  sum = total.reduce((prev, next) => prev + next,0)      
       setDatatotalPiezas(sum)
-
     },[listProductos])
 
     useEffect( () => {
@@ -92,69 +96,90 @@ function similaresScreen({navigation, route}){
         if (parseInt(data[index].cantidad) > 1) {  
           data[index].cantidad = parseInt(data[index].cantidad)-1              
           setlistProductos(data)
-        }
-        
-        
+        }        
       }
-
-     console.log(listProductos)
 
     return( 
       
         <ImageBackground source={Interface.fondo} style={{flex:1}}>
           <ScrollView >
             <View style={Interface.container}>
-                <Text style={styles.text}>{cantidad} {empaque.empaque} {producto}  Piezas: {cantidad * empaque.piezas}</Text>
-                <Text style={styles.text}>  Piezas agregadas: {totalPiezas}</Text>
+                {completo ?
+                  <TouchableOpacity>
+                    <Text style={[Interface.boton, {marginTop:5}]}>aceptar</Text>
+                  </TouchableOpacity>
+                : null
+                }
+                <Text style={styles.text}>{cantidad} {empaque.empaque} {producto}  </Text>
+                <View style={{flexDirection:"row",justifyContent:"space-around"}}>
+                    <View>
+                      <Text style={[styles.text,{fontSize:20, textAlign:"center"}]}>{cantidad * empaque.piezas}</Text>
+                      <Text style={[styles.text,{fontSize:10}]}>Piezas</Text>
+                    </View>
+                    <View>
+                      <Text style={[styles.text,{fontSize:20, textAlign:"center"}]}>{totalPiezas}</Text>
+                      <Text style={[styles.text,{fontSize:10}]}>Piezas agregadas</Text>
+                    </View>
+                </View>
                 <TextInput placeholder="Cantidad" value={cant} style={styles.input} onChangeText={(x) => changeCantidad(x)}/>
 
             </View>
 
-            <View style={{flex:1}}>
-               <FlatList 
-                    style={Interface.container}
-                    data={dataInventario}            
-                    keyExtractor={(item) =>item.claves}
-                    renderItem={({item}) => <TouchableOpacity onPress={ () => handleListaProductos(item)}>                
-                                                <Text style={styles.text}>{item.productos}</Text>
-                                            </TouchableOpacity>}
-                /> 
+            <View style={{height:120}}>
+                <View style={Interface.container}>
+                  <Text style={styles.encabezadoListas}>Productos</Text>
+                  <FlatList                         
+                        data={dataInventario}            
+                        keyExtractor={(item) =>item.claves}
+                        renderItem={({item}) => <TouchableOpacity onPress={ () => handleListaProductos(item)}>                
+                                                    <Text style={styles.text}>{item.productos}</Text>
+                                                </TouchableOpacity>}
+                    /> 
 
+              </View>
             </View >
 
-            <View style={{flex:1}}>
-                <FlatList 
-                    style={Interface.container}
-                    data={empaqueFiltrado}
-                    keyExtractor={(item) =>String(item.id)}
-                    renderItem={({item}) =>  {
-                        if (item.empaque !=='SEIS' && item.empaque !=='DOCE')
-                          return(
-                            <View style={{flexDirection:'row', justifyContent:'space-between', marginTop:10}}>                      
-                              <Text style={styles.text} >{item.empaque} - {item.precio}</Text> 
-                              <View style={{flexDirection:'row'}}>                                
-                                <AntDesign name="pluscircleo" size={24} color={Interface.colorText} onPress={ () => handleListaEmpaque(item)} />                      
-                              </View>
-                            </View>  
-                          )                                   
-                          
-                      }
-                      }
-                  />
-            </View>
+           
+            <View style={[Interface.container,{height:150}]}>
+              <Text style={styles.encabezadoListas}>Empaques</Text>
+              <FlatList                     
+                  data={empaqueFiltrado}
+                  keyExtractor={(item) =>String(item.id)}
+                  renderItem={({item}) =>  {
+                      if (item.empaque !=='SEIS' && item.empaque !=='DOCE')
+                        return(
+                          <View style={{flexDirection:'row', justifyContent:'space-between', marginTop:10}}>                      
+                            <Text style={styles.text} >{item.empaque} - {item.precio}</Text> 
+                            <View style={{flexDirection:'row'}}>                                
+                              <AntDesign name="pluscircleo" size={24} color={Interface.colorText} onPress={ () => handleListaEmpaque(item)} />                      
+                            </View>
+                          </View>  
+                        )                                   
+                        
+                    }
+                    }
+                />
+              </View>
+            
 
             <View style={{flex:1}}>
-               <FlatList 
-                    style={Interface.container}
-                    data={listProductos}            
-                    keyExtractor={(item) =>item.id}
-                    renderItem={({item}) => <View style={{flex:1,flexDirection:'row',justifyContent:'space-between', marginTop:10}}>
-                                                <Text style={styles.text}>{item.cantidad} - {item.empaque} {item.producto}</Text>
-                                                <AntDesign name="pluscircleo" size={24} color="#3F3DE0" onPress={() => aumentar(item)}/>
-                                                <AntDesign name="minuscircleo" size={24} color="#3F3DE0" onPress={() => disminur(item,parseInt(item.cantidad))}/>
-                                                <AntDesign name="delete" size={24} color="#3F3DE0" onPress={() => setlistProductos(listProductos.filter((data)=> data.id !==item.id))}/>
-                                            </View>}                                            
-                /> 
+              <View style={Interface.container}>
+                  <Text style={styles.encabezadoListas}>Listado del surtido</Text>
+                  <FlatList                         
+                        data={listProductos}            
+                        keyExtractor={(item) =>item.id}
+                        renderItem={({item}) => <View style={{flex:1,flexDirection:'row',justifyContent:'space-between', marginTop:10}}>
+                                                    <View style={{flex:3}}>
+                                                      <Text style={styles.text}>{item.cantidad} - {item.empaque} {item.producto}</Text>
+                                                    </View>
+                                                    <View style={{flex:1,flexDirection:'row',justifyContent:'space-between'}}>
+                                                      <AntDesign name="pluscircleo" size={24} color="#3F3DE0" onPress={() => aumentar(item)}/>
+                                                      <AntDesign name="minuscircleo" size={24} color="#3F3DE0" onPress={() => disminur(item,parseInt(item.cantidad))}/>
+                                                      <AntDesign name="delete" size={24} color="#3F3DE0" onPress={() => setlistProductos(listProductos.filter((data)=> data.id !==item.id))}/>
+                                                    </View>
+                                                </View>}                                            
+                    /> 
+               </View >
             </View >
           </ScrollView> 
         </ImageBackground>
@@ -172,6 +197,13 @@ const styles = StyleSheet.create({
       text:{
         color:Interface.colorText,
         fontWeight:"bold"
+      },
+      encabezadoListas:{
+        backgroundColor:Interface.colorText, 
+        color:"white", 
+        textAlign:"center",
+        borderTopEndRadius:20,
+        borderTopStartRadius:20
       }
 })
 
